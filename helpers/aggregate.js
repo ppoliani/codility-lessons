42d83944e0fa3114af2ee807b6d112e668ff8732
ppoliani <ppoliani@gmail.com>
2020-04-17 15:01:39 +0100

    submit tape equilibrium

--- helpers/aggregate.js
+++ helpers/aggregate.js
@@ -1,3 +1,4 @@
 const sum = list => list.reduce((s, item) => s + item, 0);
+const sumAbs = list => list.reduce((s, item) => s + Math.abs(item), 0);
 
-module.exports = {sum}
+module.exports = {sum, sumAbs}
