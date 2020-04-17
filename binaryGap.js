49308e89de37608164c3357f6f63d004ccc8a2d7
ppoliani <ppoliani@gmail.com>
2020-04-17 11:25:20 +0100

    sybmitteed version

--- binaryGap.js
+++ binaryGap.js
@@ -1,51 +1,82 @@
 
 const run = n => {
 
-let _stack = [];
+// const createItem = (char, count, closed) => ({char, count, closed});
+// const push = stack => item => stack.items.push(item);
+// const pop = stack => stack.items.pop();
+// const peak = stack => stack.items[stack.length - 1] || {char: '', count: 0};
 
+// const increaseCount = stack => {
+//   const top = peak(stack);
+//   pop(stack);
+//   push(createItem(top.char, top.count + 1))
+// }
+
+
+
+// const handler = {
+//   apply: (target, thisArg, args) => {
+//     return target(thisArg)(...args);
+//     // 
+//   }
+// };
+
+// const createStack = () => {
+//   const state = {items: []};
+//   const interface = {
+//     createItem,
+//     push,
+//     pop,
+//     peak,
+//     increaseCount,
+//     ...state
+//   }
+
+//   return new Proxy(interface, handler);
+// }
+
+let _stack = [];
 const createItem = (char, count, closed) => ({char, count, closed});
 const push = (item, stack=_stack) => stack.push(item);
 const pop = (stack=_stack) => stack.pop();
-const peak = (stack=_stack) => stack[stack.length - 1] || {char: '', count: 0};
-
-const increaseCount = () => {
+const peak = (stack=_stack) => stack[stack.length - 1];
+const increaseCount = (stack=_stack) => {
   const top = peak();
-  pop();
-  push(createItem(top.char, top.count + 1))
+
+  if(top) {
+    pop();
+    push(createItem(top.char, top.count + 1))
+  }
 }
-
-const closeItem = () => {
+const newEntry = () => {
   const top = peak();
-  pop();
-  push(createItem(top.char, top.count, true))
+
+  if(top && top.count !== 0) {
+    pop();
+    push(createItem(top.char, top.count, true))
+  }
+  
+  push(createItem('', 0))
 }
 
 const toBinary = n => n.toString(2);
 
 const getMaxGap = () => {
   const filtered = _stack.filter(i => i.closed)
+  
   if(filtered.length === 0) return 0;
 
   return filtered.reduce((max, curr) => {
-    return curr.count > max ? curr.count : max;
-  }, peak().count)
-}
+    return curr.count > max ? curr.count : max;``
+  },filtered[0].count)
+};
 
 const solution = n => {
   const binary = toBinary(n);
 
-  let prev = '';
-
   const findGaps = (i) => {
     if(i === 1) {
-      
-    }
-    if(i === 1 && prev === 0) {
-      closeItem();
-    }
-    else if(i === 0 && prev === 1) {
-      push(createItem(0, 1));
-      prev = 0;
+      newEntry();
     }
     else if(i === 0) {
       increaseCount();
@@ -54,8 +85,6 @@ const solution = n => {
 
   binary.split('').map(i => Number(i)).forEach(findGaps);
 
-  // console.log('>>>', _stack)
-
   return getMaxGap();
 }
 
@@ -63,7 +92,8 @@ return solution(n);
 }
 
 
-console.log(run(529))
+console.log(run(328))
+// console.log(run(529))
 // console.log(run(1041))
 // console.log(run(0))
 // console.log(run(1))
