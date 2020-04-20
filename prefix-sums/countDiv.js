15457b370e97fbd1fdc5d52df86dcc780267622c
ppoliani <ppoliani@gmail.com>
2020-04-20 13:45:17 +0100

    submit count div

--- prefix-sums/countDiv.js
+++ prefix-sums/countDiv.js
@@ -2,28 +2,16 @@ const assert = require('assert');
 
 const run = (a, b, k) => {
   const solution = (a, b, k) => {
-    if(k === 1) return (b - a) + 1;
+    const minValue = Math.floor((a + k - 1) / k) * k;
+    if(minValue > b) return 0;
 
-    let i = a;
-    let count = 0;
-
-    while(i <= b && i <= 2000000000) {
-      if(i % k === 0) {
-        count += 1;
-        i += k;
-      }
-      else {
-        i++;
-      }
-
-    }
-
-    return count;
+    return Math.floor((b - minValue) / k) + 1;
   }
 
   return solution(a, b, k);
 }
 
+assert.equal(run(0, 1, 11), 0);
 assert.equal(run(6, 11, 2), 3);
 assert.equal(run(0, 100, 2), 51);
 assert.equal(run(100, 100, 2), 1);
