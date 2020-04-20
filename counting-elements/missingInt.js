0661b2671de415997278bb9927cbdfebe0b9f945
ppoliani <ppoliani@gmail.com>
2020-04-20 12:17:35 +0100

    uncomment code

--- counting-elements/missingInt.js
+++ counting-elements/missingInt.js
@@ -22,10 +22,10 @@ const run = (arr) => {
   return solution(arr);
 }
 
-// console.log(run([3, 10]));
-// console.log(run([2]));
-// console.log(run([1, 3, 6, 4, 1, 2]));
-// console.log(run([1, 2, 3]));
-// console.log(run([-1, -3]));
-// console.log(run([-1, -3, 1, 2]));
+console.log(run([3, 10]));
+console.log(run([2]));
+console.log(run([1, 3, 6, 4, 1, 2]));
+console.log(run([1, 2, 3]));
+console.log(run([-1, -3]));
+console.log(run([-1, -3, 1, 2]));
 console.log(run([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 100, 102, 103, 112, 113]));
