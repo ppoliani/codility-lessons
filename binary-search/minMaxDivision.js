afcf5119ff937c8ad2865aaab3c2fa7483a7107e
ppoliani <ppoliani@gmail.com>
2020-05-03 17:44:53 +0100

    add min max division

---
+++ binary-search/minMaxDivision.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+
+const run = (K, M, A) => {
+
+  const solution = (K, M, A) => {
+    const check = (num, K, A) => {
+      let i = 0;
+      let sum = 0;
+
+      while(i < A.length) {
+        if(A[i] > num) return false;
+
+        sum += A[i];
+
+        if(sum > num) {
+          sum = A[i];
+          K--;
+
+          if(K === 0) return false;
+        }
+
+        i++;
+      }
+
+      return true;
+    }
+
+    let sum = 0;
+    let max = A[0];
+
+    for (let i = 0; i < A.length; i++) {
+      sum += A[i];
+      max = Math.max(A[i], max)
+    }
+
+    let beg = max;
+    let end = sum;
+    let min = sum;
+
+    while(beg <= end) {
+      const mid = Math.floor((beg + end) / 2);
+
+      if(check(mid, K, A)) {
+        min = mid;
+        end = mid - 1;
+      }
+      else {
+        beg = mid + 1;
+      }
+    }
+
+    return min;
+  }
+
+  return solution(K, M, A);
+}
+
+assert.equal(run(3, 5, [2, 1, 5, 1, 2, 2, 2]), 6);
