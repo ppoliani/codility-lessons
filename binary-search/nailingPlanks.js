a5e99c021b8cbcbaa602eedbdbddbe204023927d
ppoliani <ppoliani@gmail.com>
2020-05-04 16:54:30 +0100

    nailing planks second attempt

--- binary-search/nailingPlanks.js
+++ binary-search/nailingPlanks.js
@@ -4,12 +4,16 @@ const run = (A, B, C) => {
 
   const solution = (A, B, C) => {
     const check = nailCount => {
-      if(!nailCount) return false;
+      if(nailCount <= 0) return false;
       let covered = 0;
+      let planksNailed = {};
 
-      for (let i = 0; i < nailCount; i++) {        
+      for (let i = 0; i <= nailCount; i++) {        
         for (let j = 0; j < A.length; j++) {
-          if(C[i] >= A[j] && C[i] <= B[j]) covered += 1;
+          if(C[i - 1] >= A[j] && C[i - 1] <= B[j] && !planksNailed[j]) {
+            covered += 1;
+            planksNailed[j] = true;
+          }
         }
       }
       
@@ -24,7 +28,7 @@ const run = (A, B, C) => {
       const mid = Math.floor((beg + end) / 2);
 
       if(check(mid, A, B, C)) {
-        min = mid + 1;
+        min = mid;
         end = mid - 1; 
       }
       else {
@@ -39,5 +43,6 @@ const run = (A, B, C) => {
 }
 
 assert.equal(run([1, 4, 5, 8], [4, 5, 9, 10], [4, 6, 7, 10, 2]), 4);
-// assert.equal(run([1, 4], [4, 5], [8]), -1);
-// assert.equal(run([1, 4], [4, 5], [4]), 1);
+assert.equal(run([1, 4], [4, 5], [8]), -1);
+assert.equal(run([1, 4], [4, 5], [4]), 1);
+assert.equal(run([1, 4], [4, 6], [2, 4]), 2);
