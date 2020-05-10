f400db01c52c683b4c02c1c3ce2a8981b313ddf1
ppoliani <ppoliani@gmail.com>
2020-05-10 13:53:56 +0100

    submit min abs sum two correct solution

--- caterpillar/minAbsSumOfTwo.js
+++ caterpillar/minAbsSumOfTwo.js
@@ -1,21 +1,46 @@
 const assert = require('assert');
+const {} = require('../helpers/sort')
 
-// Naive approach; Brute force approach O(n^2. Score 45%
+// Naive approach; Brute force approach O(n^2). Score 45% (accurate but poor performance)
+// Greedy algorithm; score 72% i.e. sort and compare the first with the last item and increase back when new min found
 const run = (A) => {
 
   const solution = (A) => {
-    let min = Number.MAX_SAFE_INTEGER;
+    A = A.sort((a, b) => a - b);
+    let min = Math.abs(A[0] + A[0]);
+  
+    let l = 0;
+    let r = A.length - 1;
 
-    for (let i = 0; i < A.length; i++) {
-      const item1 = A[i];
+    while(l <= r) {
+      let lval = Math.abs(A[l] * 2);
+      let rval = Math.abs(A[r] * 2);
+      let both = Math.abs(A[l] + A[r]);
 
-      for (let j = 0; j < A.length; j++) {
-        const item2 = A[j];
-        const absSum = Math.abs(item1 + item2);
-        
-        if(absSum < min) {
-          min = absSum;
-        }
+      if(lval < min) {
+        min = lval;
+      }
+      if(rval < min) {
+        min = rval;
+      }
+      if(both < min) {
+        min = both;
+      }
+
+       //if A[l] >=0, we have the smallest number already.
+      if (A[l] >= 0){
+        return min;
+      }
+
+      if(lval < rval) {
+        r -= 1;
+      }
+      else if(lval > rval) {
+        l += 1;
+      }
+      else {
+        l += 1;
+        r -= 1;
       }
     }
 
@@ -27,3 +52,6 @@ const run = (A) => {
 
 assert.equal(run([1, 4, -3]), 1);
 assert.equal(run([-8, 4, 5, -10, 3]), 3);
+assert.equal(run([0]), 0);
+assert.equal(run([-10, -3, -1, 0, 2, 4, 5]), 0);
+
