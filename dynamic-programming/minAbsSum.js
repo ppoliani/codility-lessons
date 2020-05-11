8fd44fd5dc95490abf4f873ab1430454bf9d0253
ppoliani <ppoliani@gmail.com>
2020-05-11 19:56:49 +0100

    minAbsSum first attempt

--- dynamic-programming/minAbsSum.js
+++ dynamic-programming/minAbsSum.js
@@ -1,14 +1,55 @@
 const assert = require('assert');
-const {frog, frogRecursive} = require('../helpers/dp');
 
 const run = (A) => {
-
+  
   const solution = (A) => {
-    console.log(frog([1, 2], 5));
-    console.log(frogRecursive([1, 2], 5));
+    A = A.sort((a, b) => a - b);
+    if(A.length === 0) return 0;
+
+    const getOptions = (a, b) => {
+      return [
+        Math.abs(a * -1 + b * -1),
+        Math.abs(a * -1 + b * 1),
+        Math.abs(a * 1 + b * -1),
+        Math.abs(a * 1 + b * 1)
+      ];
+  }
+
+    const recur = (arr, n) => {
+      // if(arr.length === 1) {
+      //   console.log(`${Math.min(...getOptions(0, A[0]))}`)
+      //   return Math.min(...getOptions(0, A[0]));
+      // }
+
+      if(n === 0) {
+        return Math.min(...getOptions(0, A[0]));
+      }
+
+      if(n === 1) {
+        const min = Math.min(...getOptions(A[0], A[1]));
+        arr.splice(0, 1)
+        A[0] = min;
+
+        return min;
+      }
+
+      const lMin = recur(arr, n - 1);
+      const rMin = recur(arr, n - 2);
+
+      console.log(`lMin ${lMin}`)
+      console.log(`rMin ${rMin}`)
+      console.log(`...getOptions(lMin, rMin) ${getOptions(lMin, rMin)}`)
+
+      return Math.min(lMin, rMin);
+    }
+
+    return recur(A, A.length);
   }
 
   return solution(A);
 }
 
-assert.equal(run([]), true);
+assert.equal(run([2, 2, 1]), 1);
+assert.equal(run([1, 5, 2, -2]), 0);
+assert.equal(run([1]), 1);
+assert.equal(run([]), 0);
