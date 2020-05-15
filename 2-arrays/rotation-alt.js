c04dbb546c0c25841f5ad950fed50bfbc849875a
ppoliani <ppoliani@gmail.com>
2020-05-15 11:47:21 +0100

    rotaion-alt

---
+++ 2-arrays/rotation-alt.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+
+const run = (A, K) => {
+
+  /**
+   * Do K times the following:
+   *   - iterate through A and swap the item at 0 with the item at j + 1 as long as j + 1 doesn't cross the limit of the arry
+   */
+  const solution = (A, K) => {
+    const swap = (A, i, j) => {
+      const tmp = A[i];
+      A[i] = A[j];
+      A[j] = tmp;
+    }
+
+    const N = A.length;
+    for (let i = 0; i < K; i++) {
+      for (let j = 0; j < A.length; j++) {
+        const index = (j + 1) > N - 1 ? 0 : j + 1;
+        swap(A, 0, index);
+      }
+    }
+
+    return A;
+  }
+
+  return solution(A, K);
+}
+
+assert.deepEqual(run([3, 8, 9, 7, 6], 3), [9, 7, 6, 3, 8]);
+assert.deepEqual(run([0, 0, 0], 1), [0, 0, 0]);
+assert.deepEqual(run([1, 2, 3, 4], 4), [1, 2, 3, 4]);
+assert.deepEqual(run([], 5), []);
